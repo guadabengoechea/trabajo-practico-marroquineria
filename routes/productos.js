9ca4c50808b25a646054c4e9745f1a0a636ca748
Guadalupe Bengoechea <guadabengoechea@gmail.com>
2025-06-08 21:55:08 -0300

    crear y editar funcionando

--- routes/productos.js
+++ routes/productos.js
@@ -6,7 +6,7 @@ router.get('/', controller.obtenerProductos);
 router.get('/nuevo', controller.mostrarFormularioNuevo);
 router.post('/', controller.crearProducto);
 router.get('/editar/:id', controller.mostrarFormularioEditar);
-router.post('/:id', controller.actualizarProducto);
+router.put('/editar/:id', controller.actualizarProducto);
 router.get('/eliminar/:id', controller.eliminarProducto);
 router.delete("/destruir/:id", controller.destruirProducto);
 
